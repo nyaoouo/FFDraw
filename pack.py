bf7ce5822b51e4a2da3c21acd540fa5d4ecbda72
ny <64143453+nyaoouo@users.noreply.github.com>
2023-02-16 12:56:14 +0800

    add download guide, impl pack script

--- pack.py
+++ pack.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+import subprocess
+import time
+import zipfile
 
 os.chdir('pack_assets')
 os.system(
@@ -8,3 +11,7 @@ os.system(
     '..\main.py'
 )
 shutil.copy(r'dist/main.exe', '../FFDraw.exe')
+os.chdir('../')
+with zipfile.ZipFile(time.strftime("release/ffd_release_%Y_%m_%d_%H_%M_%S.zip"), 'w') as zf:
+    for f in subprocess.check_output("git ls-files", shell=True).decode('utf-8').splitlines():
+        zf.write(f, 'FFDraw/' + f, compresslevel=9,compress_type=zipfile.ZIP_DEFLATED)
