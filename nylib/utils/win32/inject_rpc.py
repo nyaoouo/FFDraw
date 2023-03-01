753117a152c1c24c94ee96b4884eff593fae9b98
ny <64143453+nyaoouo@users.noreply.github.com>
2023-03-01 10:48:29 +0800

    modify error handling, logic optimize

--- nylib/utils/win32/inject_rpc.py
+++ nylib/utils/win32/inject_rpc.py
@@ -6,25 +6,23 @@ import sys
 import threading
 import time
 
-from nylib.utils import Mutex, wait_until, ResEvent
+from nylib.utils import Mutex, wait_until
 from nylib.rpc.namedpipe_pickle import RpcClient
 from nylib.utils.win32 import injection, process
 
 
 class Handle:
     logger = logging.getLogger('InjectHandler')
-    res_events: dict[int, ResEvent]
 
     def __init__(self, pid, process_handle):
         self.pid = pid
         self.process_handle = process_handle
         self.pipe_name = r'\\.\\pipe\\NyLibInjectPipe-pid-' + str(pid)
         tmp_dir = pathlib.Path(os.environ['TEMP'])
-        self.exc_dir = tmp_dir / f'NyLibInjectErr{self.pid}.txt'
+        self.exc_file = tmp_dir / f'NyLibInjectErr{self.pid}-{time.time()}.txt'
         self.lock_file = Mutex(tmp_dir / f'NyLibInjectLock-{pid}.lck')
         self.client = RpcClient(self.pipe_name)
         self.is_starting_server = False
-        self.res_events = {}
         self.paths = []
 
     def is_active(self):
@@ -37,7 +35,6 @@ class Handle:
         assert not self.is_active()
         pywin32_dll_place()
         self.is_starting_server = True
-        exc_file = (self.exc_dir / f'err_{self.pid}_{time.time()}.log').absolute()
         shell_code = f'''
 def run_rpc_server_main(lock_name, pipe_name):
     import threading
@@ -61,12 +58,15 @@ try:
             sys.path.append(_p)
     run_rpc_server_main({repr(str(self.lock_file.name))}, {repr(self.pipe_name)})
 except Exception:
-    ctypes.windll.user32.MessageBoxW(0, 'error:\\n'+traceback.format_exc() ,'error' , 0x40010)
+    # ctypes.windll.user32.MessageBoxW(0, 'error:\\n'+traceback.format_exc() ,'error' , 0x40010)
+    with open({repr(str(self.exc_file))},'w',encoding='utf-8') as f:
+        f.write(traceback.format_exc())
 '''
         # compile(shell_code, 's', 'exec')
         res = injection.exec_shell_code(self.process_handle, shell_code.encode('utf-8'), True)
-        if exc_file.exists():
-            self.logger.error('error occurred in injection:\n' + exc_file.read_text('utf-8'))
+        if self.exc_file.exists():
+            self.logger.error('error occurred in injection:\n' + self.exc_file.read_text('utf-8'))
+            self.exc_file.unlink(missing_ok=True)
         elif res != 0:
             self.logger.warning(f'server fail, res:{res:x}')
         self.is_starting_server = False
@@ -80,14 +80,6 @@ except Exception:
             wait_until(self.is_active)
         if not self.client.is_connected.is_set():
             self.client.connect()
-
-    def on_run_res(self, _, args):
-        rid, is_success, data = args
-        if res := self.res_events.pop(rid, None):
-            if is_success:
-                res.set(data)
-            else:
-                res.set_exception(data)
 
     def add_path(self, path):
         if self.is_active():
@@ -114,9 +106,10 @@ def pywin32_dll_place():
         target_path = target_dir / dll_name
         if target_path.exists(): continue
         for p in sys.path:
-            if (source_path := pathlib.Path(p) / dll_name).exists():
+            if (source_path := pathlib.Path(p) / dll_name).exists() or (source_path := pathlib.Path(p) / 'pywin32_system32' / dll_name).exists():
                 break
         else:
+            logging.warning(f'can find {dll_name} in paths:\n' + '\n'.join(sys.path))
             raise Exception(f'Cant find dll {dll_name}')
         shutil.copy(source_path, target_path)
         logging.debug(f'place {source_path} => {target_path}')
@@ -134,9 +127,6 @@ def test():
     from nylib.utils.win32 import process
     process.enable_privilege()
     p_handle = process.open_process(pid := next(process.pid_by_executable(b'ffxiv_dx11.exe')))
-    d = pathlib.Path('.test_injection').absolute()
-    d.mkdir(exist_ok=True, parents=True)
-    clean_locks(d)
     print(Handle(pid, p_handle).run('import os;res=os.getpid()'))
 
 
