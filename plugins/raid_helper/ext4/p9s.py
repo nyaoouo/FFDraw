165b119310ac20d1b78c670f79ce1527be430616
ny <64143453+nyaoouo@users.noreply.github.com>
2023-05-30 21:26:28 +0800

    better Archaic Rockbreaker display

--- plugins/raid_helper/ext4/p9s.py
+++ plugins/raid_helper/ext4/p9s.py
@@ -8,7 +8,7 @@ from ff_draw.omen.effector import Effector, ScaleInEffector, FadeOutEffector
 
 from raid_helper import utils as raid_utils
 from raid_helper.utils.typing import *
-from raid_helper.data import special_actions
+from raid_helper.data import special_actions, delay_until
 
 special_actions[33337] = 0
 special_actions[33336] = 0
@@ -16,6 +16,8 @@ special_actions[33314] = 0
 special_actions[33315] = 0
 special_actions[33292] = 0
 special_actions[33139] = 0
+
+delay_until[33121] = 4
 
 p9s = raid_utils.MapTrigger(1148)
 
