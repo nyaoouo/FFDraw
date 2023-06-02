9b640798f434d79105598c98cd4041d2effd6219
ny <1>
2023-06-02 20:36:21 +0800

    add pid finding handling

--- main.py
+++ main.py
@@ -3,12 +3,30 @@ import multiprocessing
 import os
 import pathlib
 import sys
+import time
 
 exc_path = pathlib.Path(sys.executable if getattr(sys, 'frozen', False) else __file__).absolute().parent
 os.environ['ExcPath'] = str(exc_path)
 
 from nylib.logging import install
 from nylib.utils.win32.process import enable_privilege, pid_by_executable, is_admin, runas
+
+
+def find_game_pid():
+    is_log = False
+    while True:
+        pids = list(pid_by_executable(b'ffxiv_dx11.exe'))
+        if not pids:
+            if not is_log:
+                logging.debug('cant find ffxiv_dx11.exe, waiting...')
+                is_log = True
+            time.sleep(1)
+            continue
+        if len(pids) > 1:
+            logging.warning('发现多个游戏进程，请输入序号进行选择:\n' + '\n'.join(f'{i}:{pid}' for i, pid in enumerate(pids)))
+            return pids[int(input('输入序号:'))]
+        else:
+            return pids[0]
 
 
 def main():
@@ -21,13 +39,10 @@ def main():
             runas()
             exit()
         enable_privilege()
-        instance = FFDraw(next(pid_by_executable(b'ffxiv_dx11.exe')))
+        instance = FFDraw(find_game_pid())
         instance.start_sniffer()
         instance.start_gui_thread()
         instance.start_http_server()
-    except StopIteration:
-        logging.warning('我们要不先开个游戏？')
-        os.system('pause')
     except Exception as e:
         logging.critical('critical error occurred', exc_info=e)
         os.system('pause')
