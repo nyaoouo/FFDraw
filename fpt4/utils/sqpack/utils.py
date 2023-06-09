faf0470f2372cd9fd062cb65817da89689777628
ny <64143453+nyaoouo@users.noreply.github.com>
2023-06-09 15:53:38 +0800

    add map path

--- fpt4/utils/sqpack/utils.py
+++ fpt4/utils/sqpack/utils.py
@@ -21,3 +21,9 @@ def icon_path(icon_id: int, is_hq: bool = False, language: Language | str = None
     elif not language.endswith('/'):
         language += '/'
     return f"ui/icon/{icon_id // 1000:03d}000/{language}{icon_id:06d}" + ('_hr1.tex' if is_hq else '.tex')
+
+
+def map_path(sq_pack, map_id, size='m'):
+    _path = sq_pack.sheets.map_sheet[map_id].path
+    t_name, map_idx = _path.split('/', 1)
+    return f'ui/map/{t_name}/{map_idx}/{t_name}{map_idx}_{size}.tex'
