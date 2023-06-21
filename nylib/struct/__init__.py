d12faf0c82adb868f81087d557c206019b316ed3
ny <64143453+nyaoouo@users.noreply.github.com>
2023-06-21 11:12:48 +0800

    remove nylib from current repo

--- nylib/struct/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import set_fields_from_annotations, offset
-from .enum import Enum, auto as e_auto
-from .utils import fctypes, next_bit
--- nylib/utils/win32/winapi/__init__.py
+++
@@ -1,6 +0,0 @@
-from .advapi32 import *
-from .kernel32 import *
-from .ntdll import *
-from .structure import *
-from .psapi import *
-from .iphlpapi import *
