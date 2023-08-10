a03743753f634a704e2a45aaf0cf436a2d2b835d
ny <64143453+nyaoouo@users.noreply.github.com>
2023-08-10 10:50:59 +0800

    🔒 Refactor data.py with DictLock class for locking dictionary access.

--- plugins/raid_helper/data.py
+++ plugins/raid_helper/data.py
@@ -1,11 +1,32 @@
-import glm
+import inspect
 
-special_actions: dict[int, int] = {
-    # action_id: shape
-}
-delay_until: dict[int, float] = {
-    # action_id: sec
-}
-omen_color: dict[int, glm.vec4 | tuple[glm.vec4,] | tuple[glm.vec4, glm.vec4,]] = {
-    # action_id: color
-}
+
+class DictLock(dict):
+    def __init__(self):
+        super().__init__()
+        self.edit_source = {}
+
+    def assert_locked(self, key):
+        s = inspect.stack()[2]
+        callee = s.filename, s.lineno
+        if self.edit_source.setdefault(key, callee)[0] != callee[0]:
+            old_filename, old_line = self.edit_source[key]
+            raise RuntimeError(f"key {key!r} is being edited by {old_filename}:{old_line}")
+
+    def __setitem__(self, key, value):
+        self.assert_locked(key)
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key):
+        self.assert_locked(key)
+        self.edit_source.pop(key, None)
+        super().__delitem__(key)
+
+    def pop(self, key, default=None):
+        self.assert_locked(key)
+        self.edit_source.pop(key, None)
+        return super().pop(key, default)
+
+special_actions = DictLock()
+delay_until = DictLock()
+omen_color = DictLock()
