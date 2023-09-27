8064a1106db89d7d3ad2dc9b2bb14f31f22c4489
ny <64143453+nyaoouo@users.noreply.github.com>
2023-09-27 17:00:27 +0800

    🔧 Fix wrong dict is used

--- plugins/raid_helper/utils/trigger.py
+++ plugins/raid_helper/utils/trigger.py
@@ -412,8 +412,8 @@ class TriggerGroup:
     # region on_actor_control
     # add event support for overall actor control
     def _recv_on_actor_control(self, msg: ActorControlMessage):
-        for c in self._on_cancel_channel.get(msg.id, ()): call_safe(c, msg)
-        for c in self._on_cancel_channel.get(None, ()):  call_safe(c, msg)  # pair all
+        for c in self._on_actor_control.get(msg.id, ()): call_safe(c, msg)
+        for c in self._on_actor_control.get(None, ()):  call_safe(c, msg)  # pair all
 
     def on_actor_control(self, *actor_control_id):
         self.hook_map.set(main.sniffer.on_actor_control.any_call, self._recv_on_actor_control)
